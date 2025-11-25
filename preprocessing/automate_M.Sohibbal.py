8ddc0bb00a4db97f65a3aa513c61550c5f266c94
Sohibbal <m.sohibbal5312@student.unri.ac.id>
2025-11-25 08:48:43 +0700

    test5

--- preprocessing/automate_M.Sohibbal.py
+++ preprocessing/automate_M.Sohibbal.py
@@ -1,3 +1,4 @@
+# Import library
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.compose import ColumnTransformer
@@ -9,7 +10,7 @@ raw_file = os.path.join(BASE_DIR, "obesity_classification_raw.csv")
 output_folder = os.path.join(BASE_DIR, "preprocessing")
 output_file = os.path.join(output_folder, "obesity_classification_preprocessing.csv")
 
-# Dataset
+# Load Dataset
 df = pd.read_csv(raw_file)
 print("Dataset berhasil dibaca, contoh data:")
 print(df.head())
@@ -21,40 +22,30 @@ categorical_cols = df.select_dtypes(include=['object']).columns
 # Menghapus Duplikat
 df_cleaned = df.drop_duplicates()
 
-# ======================================================================
-# 1️⃣ Encoding LabelEncoder UNTUK SETIAP KOLOM KATEGORIKAL
-# ======================================================================
+# Encoding LabelEncoder Kolom Kategorikal
 label_encoders = {}
 
 for col in categorical_cols:
     le = LabelEncoder()
     df_cleaned[col] = le.fit_transform(df_cleaned[col])
-    label_encoders[col] = le   # simpan encoder jika nanti dibutuhkan
-# ======================================================================
+    label_encoders[col] = le
 
-# ======================================================================
-# 2️⃣ Scaling numerik saja dengan ColumnTransformer
-# ======================================================================
+# Scaling kolom Numerik dengan ColumnTransformer
 scaler = StandardScaler()
-
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', scaler, numerical_cols)
     ],
-    remainder='drop'   # kolom kategorikal sudah di-encode → tidak perlu transformer
+    remainder='drop'
 )
 
-# Menjalankan preprocessing
+# Menjalankan preprocessing scaling
 data_scaled = preprocessor.fit_transform(df_cleaned[numerical_cols])
 
-# ======================================================================
-# 3️⃣ Bentuk DataFrame final → gabungkan kolom kategori + hasil scaling
-# ======================================================================
+# Menggabungkan kolom numerik dan kategorikal
 df_preprocessed = df_cleaned.copy()
 df_preprocessed[numerical_cols] = data_scaled
 
-# Simpan hasil
+# Simpan hasil dalam format csv
 df_preprocessed.to_csv(output_file, index=False)
 print(f"Hasil preprocessing disimpan di: {output_file}")
-
-# trigger workflow
